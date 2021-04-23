dbc2f8e62d85c960c7ef659bbbc8fcd58c29cb51
Ковылин Василий <coherentus@gmail.com>
2021-04-23 19:15:03 +0300

    merge

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
+from django import forms
 
+from .models import Post
 
 
 class PostForm(forms.ModelForm):
